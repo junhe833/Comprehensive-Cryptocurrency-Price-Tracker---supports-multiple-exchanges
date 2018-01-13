11a16aab5063fcac9b567e615a02f6af05cd5751
Rico <jun.he.833@gmail.com>
2018-01-12 23:58:57 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,64 +1,63 @@
-
-var polo = new ccxt.poloniex();
-
-
 window.addEventListener("load", function load() {
 	/*
 	chrome.storage.sync.get("", function (data) {
 
 	});
 	 */
-	alert("background");
 
-	
-var bitstamp = new ccxt.bitstamp();
+	var localStorage = window.localStorage;
+	var selectRegion = document.getElementById("selectRegion");
+	var selectExchangeMarkets = document.getElementById("selectExchangeMarkets")
+		var selectCryptocurrency = document.getElementById("selectCryptocurrency");
 
-var selectRegion = document.getElementById("selectRegion");
-var selectExchangeMarkets = document.getElementById("selectExchangeMarkets")
-var selectCryptocurrency = document.getElementById("selectCryptocurrency");
-
-var exchangeMarkets = {}
+	var exchangeMarkets = {};
 
 	console.log(ccxt);
-	//polo.fetchCurrencies();
-	console.log(new ccxt['poloniex']);
+
+	//localStorage.setItem('exchanges',ccxt.exchanges);
+
+	initialize();
 
 }, false);
 
+var initialize = function () {
+	//initialize map
+	var exchanges = ccxt.exchanges;
+	for (let i = 0; i < exchanges.length; i++) {
 
+		//console.log(exchanges[i]+':[\''+(new ccxt[exchanges[i]]).countries + '\']');
 
-function hello() {
-	alert("hello");
+	}
+
 }
 
-function getTicker() {
-	return polo.fetchTickers();
-}
-
-var fetchCurrency = async function fetCurrency(market){
+var fetchCurrency = async function fetCurrency(market) {
 	return market.fetchCurrencies();
 }
 
-var fetchCurrencies = async function fetchCurrencies(exchangeMarkets){
-	for(let i=0; i<exchangeMarkets.length;i++){
-		try{
-			let exchangeName = exchangeMarkets[i];
+var fetchCurrencies = async function fetchCurrencies(selectedMarkets) {
+	for (let i = 0; i < selectedMarkets.length; i++) {
+		try {
+			let exchangeName = selectedMarkets[i];
+
 			let market = exchangeMarkets[exchangeName];
-			if(market==null){
+			if (market == null) {
 				exchangeMarkets[exchangeName] = new ccxt[exchangeName];
 			}
+
 			var currencies = await fetchCurrency(market);
-			console.log(exchangeName,currencies);
-		}catch(error){
-			console.log(exchangeMarkets[i] + " does not support fetchCurrencies()");
+			exchangeMarkets[exchangeName]['currencies'] = currencies;
+
+			console.log(exchangeName, currencies);
+		} catch (error) {
+			console.log(selectedMarkets[i] + " does not support fetchCurrencies()");
 		}
 	}
 	return
 }
 
-
-function populateDropdown(selectElement, dataList){
-	for(let i=0; i<dataList.length;i++){
-		selectElement.add(new Option(dataList[i],dataList[i]));
+function populateDropdown(selectElement, dataList) {
+	for (let i = 0; i < dataList.length; i++) {
+		selectElement.add(new Option(dataList[i], dataList[i]));
 	}
 }
