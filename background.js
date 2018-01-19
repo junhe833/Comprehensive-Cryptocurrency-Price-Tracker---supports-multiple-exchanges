b7d8bfff59914002427ab10d724b8e00f95f9811
Rico <jun.he.833@gmail.com>
2018-01-19 17:07:19 -0500

    Add files via upload
    This is an older version that do not use dataTable to display the data.

--- background.js
+++ background.js
@@ -3,9 +3,7 @@ var selectedExchangeMarketSet = new Set();
 var selectCryptocurrency = new Set();
 var exchangeMarkets = {};
 
-var marketInterval = {};
-
-window.addEventListener("load", function load() {
+window.addEventListener("load",  function load() {
 	/*
 	chrome.storage.sync.get("", function (data) {
 
@@ -14,87 +12,25 @@ window.addEventListener("load", function load() {
 	var localStorage = window.localStorage;
 	 */
 
-	initializeSelectRegion();
-	/*
-	var exchanges = ccxt.exchanges;
-	console.log(exchanges);
+	// fetchMarketsCurrencies(['okex']);
 
-	for (let market in exchanges) {
-		console.log('For',market, '1 request every ', (new ccxt[market])['rateLimit']/1000, ' seconds.');
-	}*/
+	initializeSelectRegion();
+	
+	
+	
 }, false);
 
-function updateDataTable(market, tickerData) {
-	if (!$("table[id='" + market + "']")) {
-		//console.log('updateDataTable', market, " does not exist");
-		return;
-	}
-
-	let marketDiv = document.getElementById(market + "Div");
-	marketDiv.style.display = "initial";
-	let sel = marketDiv.querySelector("select");
-
-	let table = null;
-	if (!$.fn.DataTable.isDataTable("table[id='" + market + "']")) {
-		//console.log("not datatable");
-		table = $("table[id='" + market + "']").DataTable({
-				"pageLength": 100,
-				columns: [{
-						data: "name",
-						title: "Symbol"
-					}, {
-						data:"last",
-						title: "Last"
-					},{
-						data: "high",
-						title: "High"
-					}, {
-						data: "low",
-						title: "Low"
-					}, {
-						data: "percent",
-						title: "Percent Change (%)"
-					}
-				],
-				responsive: true
-			});
-	}
-
-	let list = [];
-	for (let ticker in tickerData) {
-		if (tickerData.hasOwnProperty(ticker)) {
-			let data = tickerData[ticker];
-
-			let last = data.last?data.last:'not supported';
-			let high = data.high? data.high:'not supported';
-			let low = data.low? data.low:'not supported';
-			let name = data.symbol ? data.symbol :'not supported';
-			let percent = data.percentage ? data.percentage:'not supported';
-
-			let opt = sel.querySelector("option[value='" + name + "']");
-
-			if (opt.selected) {
-				list.push({
-					"name": name,
-					"last":last,
-					"high": high,
-					"low": low,
-					"percent": percent
-				});
-			} else {}
-		}
-	}
-	$("table[id='" + market + "']").DataTable().clear().rows.add(list).draw();
-}
-
 var initializeSelectRegion = function () {
+	//populate it first with region
+	//addDropdown(document.getElementById("selectRegion"), Object.keys(countries).sort());
+
 	addDropdown(document.getElementById("selectRegion"), Object.keys(countries));
 
 	//add event for list to populate exchange markets in response to selecting regions
 	document.getElementById("selectRegion").addEventListener('change', function (event) {
 		let regionsChanged = selectHandler(event, selectedRegionSet);
 		//console.log(regionsChanged);
-		
+
 		for (let country in regionsChanged) {
 			if (regionsChanged[country] == "added") {
 				//console.log('adding', countries[country]);
@@ -104,8 +40,9 @@ var initializeSelectRegion = function () {
 				let dataList = countries[country];
 				for (let i = 0; i < dataList.length; i++) {
 					let val = dataList[i];
-					let selectElement = document.getElementById("selectExchangeMarkets");
-					let opt = selectElement.querySelector("#" + val);				
+					let selectElement = event.target;
+					let opt = selectElement.querySelector("#" + val);
+
 					let exchangesArray = ccxt.exchanges[val];
 					if (selectElement.contains(opt) && isUniqueSet(selectedRegionSet, exchangesArray)) {
 						selectElement.removeChild(opt);
@@ -122,46 +59,17 @@ var initializeSelectRegion = function () {
 		console.log('Markets affected:', marketsChanged);
 		for (let market in marketsChanged) {
 			try {
-				if (marketsChanged[market] == "added") {
-					let currencies = null;
-					if (exchangeMarkets[market]) {
-						currencies = exchangeMarkets[market]['currency'];
-					} else {
-						exchangeMarkets[market] = new ccxt[market];
-						currencies = await(exchangeMarkets[market]).fetchTickers();
-						exchangeMarkets[market]['currency'] = currencies;
-					}
+				let currencies = await(new ccxt[market]).fetchTickers();
+				console.log('currencies:', market, currencies);
 
-					console.log('currencies:', market, currencies);
-
-					let moduleDiv = createTickerContentDiv(document.getElementById("tableOfContent"), market, currencies, {});
-					marketInterval[market] = window.setInterval(async function () {
-							console.log('interval:', exchangeMarkets, market);
-							if (!exchangeMarkets[market]) {
-								exchangeMarkets[market] = new ccxt[market];
-							}
-							let cur = await(exchangeMarkets[market]).fetchTickers();
-							console.log('Request interval for market=',market,' is 1 request every ', exchangeMarkets[market]['rateLimit']/1000,' sec.');
-							exchangeMarkets[market]['currency'] = cur;
-							updateDataTable(market, cur);
-						}, exchangeMarkets[market]['rateLimit']*10, market);
-						
-					updateDataTable(market, currencies);
-
-				} else {
-					let marketDiv = document.getElementById(market + "Div");
-					if (marketDiv) {
-						//clear market table display and remove intervalCall for ticker
-						marketDiv.style.display = "none";
-						let intervalID = marketInterval[market];
-						clearInterval(intervalID);
-
-					}
+				if (currencies) {
+					if (marketsChanged[market] == "added") {
+						let obj = {};
+						let moduleDiv = createTickerContentDiv(document.getElementById("tableOfContent"), market, currencies, obj);
+					} else {}
 				}
-
 			} catch (error) {
-				//console.log(error);
-				alert(error);
+				console.log(error);
 			}
 		}
 
@@ -245,7 +153,7 @@ function createTickerContentDiv(parentElement, market, tickerData, filterOject =
 	}
 
 	let moduleDiv = document.createElement("div");
-	moduleDiv.id = market + "Div";
+	moduleDiv.id = market;
 	let h2 = document.createElement("h2");
 	h2.innerHTML = 'Market: ' + market;
 	let sel = document.createElement("select");
@@ -256,12 +164,30 @@ function createTickerContentDiv(parentElement, market, tickerData, filterOject =
 	dataDiv.id = market + "Data";
 
 	//header for table div
-	let tableDiv = document.createElement("table");
-	tableDiv.id = market;
-	tableDiv.className = "cell-border";
-	dataDiv.appendChild(tableDiv);
+	let tableHeader = document.createElement("div");
+	appendChildrenRowDiv(tableHeader, ["Currency", "High", "Low", "Percent Change"]);
+	dataDiv.appendChild(tableHeader);
 
-	let selectSet = new Set();
+	//add a eventlistener to control each coin display
+	sel.addEventListener('change', function (event) {
+		let options = event.target.options;
+		for (var i = 0, n = options.length; i < n; ++i) {
+			let val = options[i].value;
+
+			//dataDiv does not exist already then add
+			let dataDiv = event.target.parentElement.querySelector("div[id='" + val + "']");
+			console.log("SelectChangeListener", dataDiv);
+			if (options[i].selected && dataDiv == null) {
+				let rowDiv = document.createElement("div");
+				rowDiv.id = name;
+				appendChildrenRowDiv(rowDiv, [name, high, low, percent]);
+				dataDiv.appendChild(rowDiv);
+
+			} else if (!options[i].selected && dataDiv != null) {
+				dataDiv.style.display = "none";
+			}
+		}
+	});
 
 	for (let ticker in tickerData) {
 		if (tickerData.hasOwnProperty(ticker)) {
@@ -278,23 +204,20 @@ function createTickerContentDiv(parentElement, market, tickerData, filterOject =
 				opt.selected = false;
 			} else {
 				opt.selected = true;
-				selectSet.add(opt);
+				let rowDiv = document.createElement("div");
+				rowDiv.id = name;
+				appendChildrenRowDiv(rowDiv, [name, high, low, percent]);
+				dataDiv.appendChild(rowDiv);
 			}
 			sel.add(opt);
 		}
 	}
 
-	//add a eventlistener to control each coin display
-	sel.addEventListener('change', function (event) {	
-		let market = event.target.parentElement.id.replace("Div", "");
-		console.log('select change callback with market=', market);	
-		updateDataTable(market, exchangeMarkets[market]['currency']);
-	});
-
 	moduleDiv.appendChild(h2);
 	moduleDiv.appendChild(sel);
 	moduleDiv.appendChild(dataDiv);
 	parentElement.appendChild(moduleDiv);
+
 	return moduleDiv;
 }
 
